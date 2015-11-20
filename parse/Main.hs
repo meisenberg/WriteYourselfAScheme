fe5bf7a448ac7289f0073d40f8f0bb7898707aba
Michael Eisenberg <meisenberg@pandora.com>
2015-11-19 16:52:15 -0800

    Evaluation of primitives, copy/pasted from the tutorial

--- parse/Main.hs
+++ parse/Main.hs
@@ -14,7 +14,6 @@ data LispVal = Atom String
         | String String
         | Bool Bool
         | Character Char
-        deriving (Show)
              
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~" 
@@ -131,16 +130,63 @@ parseExpr = parseAtom
                 x <- try parseList <|> parseDottedList
                 char ')'
                 return x
-                            
+
+showVal :: LispVal -> String
+showVal (String contents) = "\"" ++ contents ++ "\""
+showVal (Atom name) = name
+showVal (Number contents) = show contents
+showVal (Bool True) = "#t"
+showVal (Bool False) = "#f"
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+-- make the LispVal class an instance of the Show class
+instance Show LispVal where show = showVal
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+                 
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) (\x -> x $ args) $ lookup func primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+              
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n :: [(Integer, String)] in 
+                           if null parsed 
+                              then 0
+                              else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
+              
+                                          
 parserRunner :: Show a => Parser a -> String -> String -> String
 parserRunner parser label input = case parse parser label input of
         Left err -> "No match: " ++ show err
         Right val -> "Found value: " ++ show val 
      
-readExpr :: String -> String
-readExpr input = parserRunner parseExpr "lisp" input
-                              
+readExpr :: String -> LispVal
+readExpr input = case parse parseExpr "lisp" input of
+        Left err -> String $ "No match: " ++ show err
+        Right val -> val
+                                      
 main :: IO ()
-main = do 
-        (expr:_) <- getArgs
-        putStrLn (readExpr expr)
+main = getArgs >>= print . eval . readExpr . head        
